672d0fe63233f259b6cf3c4c6cd5fd1f7e5c5ba4
cliangcliang0 <cliangcliang@qq.com>
2017-07-17 12:24:31 -0400

    sql.py add class downloader

--- sql.py
+++ sql.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+import requests
+import re,random
+
+
 class UrlManager(object):
     def __init__(self):
         self.new_urls = set()
@@ -24,6 +28,7 @@ class UrlManager(object):
     def get_new_url(self):
         new_url = self.new_urls.pop()
 
+'''
 a = UrlManager()
 print a
 a.add_new_url('www.baidu.com')
@@ -36,3 +41,49 @@ print a.has_new_url()
 #print a.get_new_url()
 print a.new_urls
 print a.old_urls
+'''
+
+class Downloader(object):
+    def get(self,url):
+        r = requests.get(url,timeout = 10)
+        if r.status_code != 200:
+            return None
+        _str = r.text
+        return _str
+
+    def post(self,url,data):
+        r = requests.post(url,data)
+        _str = r.text
+        return _str
+
+    def download(self,url,htmls):
+        if url is None:
+            return None
+        _str = {}
+        _str["url"] = url
+        try:
+            r = requests.get(url,timeout = 10)
+            if r.status_code != 200:
+                return None
+            _str["html"] = r.text
+        except Exception as e:
+            return None
+        htmls.append(_str)
+
+'''
+h = []
+d = Downloader()
+t = d.get('http://www.baidu.com')
+t1 = d.post('http://www.baidu.com','123456')
+t2 = d.get('http://t66y.cn')
+t3 = d.download('http://www.baidu.com',h)
+print t
+print '============================================'
+print t1
+print '============================================'
+print "t2 =" +  t2
+print '============================================'
+print t3
+print '============================================'
+print h
+'''
